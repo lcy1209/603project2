c131b1a34374f30e74b9ea4e712f16339f67ed39
dlcnddyd <fdsa1209@gmail.com>
2025-03-12 18:19:46 +0900

    ppt 재료들

---
+++ 쿼리 #2.sql
@@ -0,0 +1,4 @@
+INSERT INTO banners (image_url, link_url, alt) VALUES 
+('/images/banners/banner_1.png', 'https://example1.com', 'Banner 1'),
+('/images/banners/banner_2.png', 'https://example2.com', 'Banner 2'),
+('/images/banners/banner_3.png', 'https://example3.com', 'Banner 3');
